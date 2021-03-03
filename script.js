976ea35474897876ab00464830a34726cf05330d
lingi524 <lingi524@student.liu.se>
2021-03-03 19:04:13 +0100

    Got the API up and running. Connected the city name and temperature to the HTML.

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-//Get longitude and lattitude
 window.addEventListener("load", ()=> {
-    let long;
-    let lat;
-    console.log("hallåejkfn ");
-    
 
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-          console.log(position)
-          long = position.coords.longitude;
-          lat = position.coords.latitude;
-        });
-        console.log(lat);
+  let long
+  let lat
 
-    }
+  let locationCity = document.querySelector(".location-city");
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  
+  
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        long = position.coords.longitude
+        lat = position.coords.latitude
+        const api = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${long}&key=13376e09cd864dfa9576fc020d5c5b65`;
+        console.log(lat, long)
+
+        fetch(api)
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+            console.log(data);
+            const cityName = data.data[0].city_name;
+            const currentTempereature = data.data[0].temp;
+            console.log(cityName);
+            locationCity.innerText = cityName;
+            temperatureDegree.innerText = currentTempereature;
+          })
+      },
+    )
+  }
 });
-
-console.log("hallå");
