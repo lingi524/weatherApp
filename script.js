a8ed6cd6ecb6478982ce8514b26069322e3f3ff0
lingi524 <lingi524@student.liu.se>
2021-03-23 17:35:29 +0100

    Added an if statement to tell me about the current temperature.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener("load", ()=> {
 
   let locationCity = document.querySelector(".location-city");
   let temperatureDegree = document.querySelector(".temperature-degree");
-  
+  let temperatureDescription = document.querySelector(".temperature-description");
   
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
@@ -23,11 +23,28 @@ window.addEventListener("load", ()=> {
             console.log(data);
             const cityName = data.data[0].city_name;
             const currentTempereature = data.data[0].temp;
-            console.log(cityName);
+            // const icon = data.data[0].weather.icon;
             locationCity.innerText = cityName;
             temperatureDegree.innerText = currentTempereature;
+            //Get rain and snow and wind maybe, make into a boolean, when advice after that as well. 
+
+            //See if I think its warm enough for a bikeride
+            
+            if (currentTempereature < 0) {
+              temperatureDescription.innerText = "Nope. It's to cold in " + cityName + " today";
+            } else if (currentTempereature>0 && currentTempereature<15) {
+              temperatureDescription.innerText = "Hmmm, maybe if you put on a pair of långkalsonger";
+            }
+            else if (currentTempereature>15 && currentTempereature<20) {
+              temperatureDescription.innerText = "The temperature is perfect for a bikeride!";
+            }
+            else if (currentTempereature>20 && currentTempereature<30) {
+              temperatureDescription.innerText = "It's very varm outside, be sure to bring water and it will be ok!";
+            }
+
           })
       },
     )
   }
+
 });
